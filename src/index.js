c3e28d4b92e00b89d9aba0fb4984332ae18bcc15
Josh Gretz <jgretz@truefit.io>
2019-01-09 15:27:26 -0500

    factor out scene concept so we can start to make more

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import {SceneManager, findPixelKits} from './sdk';
-import {createBlankFrame} from './frames';
-import {HEIGHT, WIDTH} from './constants';
-
-const oneDot = (x, y, color) => {
-  const frame = createBlankFrame();
-  frame[x][y] = color;
-
-  return frame;
-};
+import playRainDropScene from './scenes/raindropScene';
 
 const main = async () => {
   const pixels = await findPixelKits();
   const sceneManager = pixels.map(pixel => new SceneManager(pixel));
 
-  for (let i = 0; i < HEIGHT; i++) {
-    for (let j = 0; j < WIDTH; j++) {
-      sceneManager.forEach(sm => {
-        sm.pushFrame(oneDot(j, i, '#ff0000'));
-      });
-    }
-  }
+  playRainDropScene(sceneManager);
 };
 main();
---
+++ src/scenes/raindropScene.js
@@ -0,0 +1,33 @@
+import {createBlankFrame} from '../frames';
+import {HEIGHT, WIDTH, REFRESH_RATE} from '../constants';
+
+const COLORS = ['#ff0000', '#00ff00', '#0000ff'];
+
+const oneDot = (x, y, color) => {
+  const frame = createBlankFrame();
+  frame[x][y] = color;
+
+  return frame;
+};
+
+const rainDropScene = sceneManager => {
+  let index = 0;
+
+  for (let j = 0; j < WIDTH; j++) {
+    for (let i = 0; i < HEIGHT; i++) {
+      const frame = oneDot(j, i, COLORS[index % COLORS.length]);
+      index++;
+
+      sceneManager.forEach(sm => {
+        sm.pushFrame(frame);
+      });
+    }
+  }
+};
+
+export default sceneManager => {
+  rainDropScene(sceneManager);
+  setInterval(() => {
+    rainDropScene(sceneManager);
+  }, HEIGHT * WIDTH * REFRESH_RATE);
+};
